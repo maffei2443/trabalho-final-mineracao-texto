d81937ed892cc4105ba503cffcc337a2fb561104
maffei2443 <leoitu22hotmail.com@gmail.com>
2019-06-23 10:54:50 -0300

    debugando; otimizando, removendo deprecated-functions

--- src/kmeanspp.py
+++ src/kmeanspp.py
@@ -1,7 +1,7 @@
 import numpy as np
 import operator
 from numpy.random import rand
-from numpy.random import random_integers
+from numpy.random import randint
 from itertools import accumulate
 from numpy import linalg as LA  # norma vetor
 
@@ -14,41 +14,45 @@ def sorteio(probab_vet):
 def sorteio_opt(probab_vet):
   return np.nonzero(
     np.add.accumulate( 
-        np.array(probab_vet) >= rand()
+        np.array(probab_vet, copy=False) >= rand()
   ))[0][0]
 
 class KMeansPP:
   def __init__(self, k, data):
     self.k = k
     self._data = data
-    self._dataLen = len(data)
-    self._lastCentroidIndex = None
-    self._centroidsIndex = np.empty( k )
-    self._centroids = np.empty( self._dataLen )
+    self._dataLen = dataLen= len(data)
+    self._centroidsIndex = np.empty( k, dtype=np.uint16 )
+    self._centroids = np.empty( dataLen, dtype=(np.float64, len(self._data[0])))
     self._computedCentroids = 0
-    self._probab = np.full( (k, self._dataLen), -np.inf )
-    self._distanceToCentroid = np.full( (k, self._dataLen), np.inf ) # ditancias até qqer centroid inicialmente é infinita
+    self._probab = np.full( (k, dataLen), -np.inf )
+    self._distanceToCentroid = np.full( (k, dataLen), np.inf, dtype=np.float64 ) # ditancias até qqer centroid inicialmente é infinita
+  def _computeAndSetFirstCentroid(self):
+    """Sorteia um número no inervalo [0, tamanho(vetor_de_dados) e seta o primeiro centróide para tal.
+    
+    Como efeito colateral, altera o contador de centróides computados, bem como
+    modifica os arrays de índices de centróides e de centróides.
+    """
+    index = randint(self._dataLen)
+    self._centroidsIndex[0] = index
+    self._centroids[0] = self._data[index]
+    self._computedCentroids += 1
+
   def InitCentroids(self):
     """Seta a lista de centroids iniciais utilizando o procedimento descrito no artigo original do k-means++.
     """
-    if not self._lastCentroidIndex:
-      self._lastCentroidIndex = random_integers(len(self._data)) - 1
-      
-      self._centroidsIndex[0] = self._lastCentroidIndex
-      self._centroids[0] = self._data[self._lastCentroidIndex]
-
-      self._computedCentroids += 1
+    if not self._computedCentroids:
+      self._computeAndSetFirstCentroid()
       # Generate the others centroids
       for idx in range(1,self.k):
         pos = self.computeNextCentroid()
         self._centroidsIndex[idx] = pos
         self._centroids[pos] = self._data[pos]
 
-
   def computeNextCentroid(self):
     """Computes distances from all points to last centroid"""
     self._distanceToCentroid[self._computedCentroids-1] = LA.norm(
-      self._data - self._data[self._lastCentroidIndex],
+      self._data - self._data[self._centroidsIndex[self._computedCentroids-1]],
       axis = 1
     )
     vetDx = self._distanceToCentroid.min(axis = 1)  # mihnima distancia
